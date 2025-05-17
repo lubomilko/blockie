d89529d4f0696b42ffb9a3c3888b1f4b1deb3eaf
Lubomir Milko <milko.lubomir@gmail.com>
2025-05-17 11:14:02 +0200

    Add more elementary demos.

--- samples/demo_basics.py
+++ samples/demo_basics.py
@@ -12,33 +12,64 @@ def demo_hello() -> None:
     print(blk.content)
 
 
-def demo_hello_world() -> None:
+def demo_set_var() -> None:
     blk = blockie.Block("<WORD1> <WORD2>")
     blk.fill({"word1": "Hello", "word2": "world!"})
     print(blk.content)
 
 
-def demo_sententce_hello_world() -> None:
-    blk = blockie.Block("<SENTENCE><WORD> </SENTENCE>")
-    blk.fill({"sentence": [{"word": "Hello"}, {"word": "world!"}]})
-    print(blk.content)
-
-
-def demo_date_single() -> None:
+def demo_set_block() -> None:
     blk = blockie.Block("<DATE><DAY> <MONTH></DATE>")
     blk.fill({"date": {"day": 24, "month": "December"}})
     print(blk.content)
 
 
-def demo_date_multi() -> None:
+def demo_set_block_clones() -> None:
     blk = blockie.Block("<DATE><DAY> <MONTH>\n</DATE>")
     blk.fill({"date": [{"day": 24, "month": 12}, {"day": 25, "month": 12}]})
     print(blk.content)
 
 
+def demo_set_block_as_is() -> None:
+    blk = blockie.Block("<DATE>24 December</DATE>")
+    blk.fill({"date": True})
+    print(blk.content)
+
+
+def demo_set_block_vari_1() -> None:
+    date_dict = {"day": 24, "month": "December"}
+    date_dict["vari_idx"] = 0 if isinstance(date_dict["month"], int) else 1
+
+    blk = blockie.Block("<DATE><DAY>.<MONTH>.<^DATE><DAY> <MONTH></DATE>")
+    blk.fill({"date": date_dict})
+    print(blk.content)
+
+
+def demo_set_block_vari_2() -> None:
+    blk = blockie.Block("<DATE>24.12.<^DATE>24 December</DATE>")
+    blk.fill({"date": 1})
+    print(blk.content)
+
+
+def demo_remove_var() -> None:
+    blk = blockie.Block("<NAME> <MIDNAME> <SURNAME>")
+    blk.fill({"name": "Patrick", "midname": None, "surname": "Bateman"})
+    print(blk.content)
+
+
+def demo_remove_block() -> None:
+    blk = blockie.Block("<NAME> <MIDNAME_WRAP><MIDNAME> </MIDNAME_WRAP><SURNAME>")
+    blk.fill({"name": "Patrick", "surname": "Bateman", "midname_wrap": None})
+    print(blk.content)
+
+
 if __name__ == "__main__":
     demo_hello()
-    demo_hello_world()
-    demo_sententce_hello_world()
-    demo_date_single()
-    demo_date_multi()
+    demo_set_var()
+    demo_set_block()
+    demo_set_block_clones()
+    demo_set_block_as_is()
+    demo_set_block_vari_1()
+    demo_set_block_vari_2()
+    demo_remove_var()
+    demo_remove_block()
