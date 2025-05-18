9a932b6a878ce6f17cf143b27be03e6c344e1112
Lubomir Milko <milko.lubomir@gmail.com>
2025-05-18 12:30:50 +0200

    Improve the test bat commands.

--- test/_run_tests.bat
+++ test/_run_tests.bat
@@ -1,9 +1,9 @@
-:: Store the current working dir and switch to the directory where this bat file is located, because all paths below are relative to this dir.
-:: %~dp0 contains the dir path of this bat file.
+REM Store the current working dir and switch to the directory where this bat file is located, 
+REM because all paths in tests are relative to the test dir.
 @pushd %~dp0
 
-:: Execute tests. -s: disable all stdout/stderr capturing, -v: verbose
+REM Execute tests. -s: disable all stdout/stderr capturing, -v: verbose
 pytest test.py -s -v
 
-:: Switch back to the original current working directory.
+REM Switch back to the original current working directory.
 @popd
