6fd56fe79ffa9098ea634f01603d165b9d266139
Lubomir Milko <lmilko@danfoss.com>
2025-08-03 13:05:07 +0200

    Update test running script to powershell.

--- tests/_run_tests.bat
+++
@@ -1,9 +0,0 @@
-REM Store the current working dir and switch to the directory where this bat file is located, 
-REM because all paths in tests are relative to the test dir.
-@pushd %~dp0
-
-REM Execute tests. -s: disable all stdout/stderr capturing, -v: verbose
-pytest test.py -s -v
-
-REM Switch back to the original current working directory.
-@popd
