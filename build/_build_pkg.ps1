cc1efd9ee5cabf2b49f48aa903d70fa28c754b69
Lubomir Milko <lmilko@danfoss.com>
2025-12-23 07:30:49 +0100

    Make the build script executabble from any dir.

--- build/_build_pkg.ps1
+++ build/_build_pkg.ps1
@@ -1,3 +1,6 @@
+# Store the current working directory and change it to the tool's root dir.
+Push-Location -Path (Join-Path -Path $PSScriptRoot -ChildPath '..')
+
 # Set paths to the directories with built distribution packages and with egg-info files.
 $BuildPaths =   (Join-Path -Path $PSScriptRoot -ChildPath '..\dist'),
                 (Join-Path -Path $PSScriptRoot -ChildPath "*.egg-info")
@@ -16,4 +19,7 @@ python -m pip install --upgrade pip
 # Install build package to ensure it can be executed.
 python -m pip install build
 # Build new package distribution files.
-& python -m build
+python -m build
+
+# Switch back to the original current working directory.
+Pop-Location
